bf14196e879a5634865ef55eb7b4145da6c37636
Diego Barros <diegobsilveira@gmail.com>
2017-06-07 16:38:43 -0300

    Header para arquivo de estrutura de dados
    Criação do header que define uma estrutura de dados - a árvore binária  - para o problema.

---
+++ data_structures/binary_tree.h
@@ -0,0 +1,10 @@
+#ifndef BINARY_TREE_H_ 
+#define BINARY_TREE_H_ 
+ 
+typedef struct tree_node { 
+        char* name;             //Nome da folha  
+        node left_node;         //Folha da esquerda 
+        node right_node;        //Folha da direita 
+} tree_node;
+ 
+#endif
