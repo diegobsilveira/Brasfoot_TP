0564e53a0e3b8c43e99789a424c2de934ff84163
Igor Henrique <razrplay5@gmail.com>
2017-06-10 22:57:28 -0300

    FON

--- run.C
+++ run.C
@@ -11,11 +11,29 @@ typedef struct time{
 
 int main(){
 	
+	//printf("FUTIBRAS DA INTERFACE CAGADA");
+	system("COLOR 2E");
+	FILE *fMenu;
+	fMenu = fopen("data/menu.txt","r");
+	char buf[100];
+	
+	while(!feof(fMenu)){
+		fgets(buf,100,fMenu);
+		printf("\n\t\t %s",buf);
+	}
+	
+	
+	printf("\n\n\t\t\t A P E R T E  U M A  T E C L A  P A R A  C O N T I N U A R\n\t\t\t ");
+	getch();   
+	
+	
+	
+	
+	
 	Time times[MAX];
 	
 	FILE *fTimes;
 	fTimes = fopen("data/times.txt","r");
-	char buf[40];
 	
 	if(fTimes == NULL){
 		printf("ERR - Arquivos estão faltando para a execução do programa.");	
